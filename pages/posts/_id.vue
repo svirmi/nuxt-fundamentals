3630c44adb5b9970356a912f1c9a49447909c419
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-01-04 17:08:01 +0200

    use object in nuxt-link

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -10,7 +10,7 @@
     <h3>Posts you might alos like:</h3>
     <ul>
       <li v-for="related in relatedPosts">
-        <nuxt-link :to="`/posts/${related.id}`">{{related.title}}</nuxt-link>
+        <nuxt-link :to="{name: 'posts-id', params: {id: related.id}}">{{related.title}}</nuxt-link>
       </li>
     </ul>
   </aside>
